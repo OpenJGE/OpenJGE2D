d43484f4048146afa2ad967a48a641ec03d18fce
openjge-dev <openjge.dev@gmail.com>
2019-11-02 09:46:42 -0400

    Added OGLCommands class

---
+++ src/main/java/OpenGL/OGLCommands.java
@@ -0,0 +1,74 @@
+package OpenGL;
+
+import org.lwjgl.opengl.GL11;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
+import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+
+/**
+ * The <code>OGLCommands</code> class holds all functions for performing OpenGL rendering actions.
+ */
+public class OGLCommands {
+
+    /**
+     * Draws OpenGL elements.
+     *
+     * @param data The <code>CommandData</code> object. Must contain <code>int[] textureId</code>,
+     *             <code>int[] textureUnit</code>, <code>int vaoID</code>, and <code>int vertexCount</code>
+     */
+    public void glDrawElements(CommandData data) {
+        // Bind VAO
+        glBindVertexArray(data.vaoId);
+        // We assume that there are EBOs at the first and second index of the VAO. This is in conjunction with the
+        // Mesh class. However, if a custom mesh implementation is used, then this function would need to be overridden.
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        // Bind texture(s)
+        for (int i = 0; i < data.textureId.length; i++) {
+            int textureId = data.textureId[i];
+            int textureUnit = data.textureUnit[i];
+            glActiveTexture(GL_TEXTURE0 + textureUnit);
+            glBindTexture(GL_TEXTURE_2D, textureId);
+        }
+        GL11.glDrawElements(GL_TRIANGLES, data.vertexCount, GL_UNSIGNED_INT, 0);
+        // Unbind texture(s)
+        for (int i = 0; i < data.textureId.length; i++) {
+            glActiveTexture(GL_TEXTURE0 + data.textureUnit[i]);
+            glBindTexture(GL_TEXTURE_2D, 0);
+        }
+        // Unbind VAO
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(0);
+        glBindVertexArray(0);
+    }
+
+    /**
+     * The <code>CommandData</code> class holds all fields required to execute OpenGL commands in the
+     * <code>OGLCommands</code> class.
+     */
+    public static class CommandData {
+        /**
+         * Collection of texture IDs, with each index representing a different texture. Must correspond with
+         * textureUnit array.
+         */
+        public int[] textureId;
+        /**
+         * Collection of texture units, with each index representing a different texture unit. Must correspond with
+         * textureId array.
+         */
+        public int[] textureUnit;
+        /**
+         * The VAO ID of the mesh being rendered.
+         */
+        public int vaoId;
+        /**
+         * The number of vertices in the mesh being rendered.
+         */
+        public int vertexCount;
+    }
+
+}
